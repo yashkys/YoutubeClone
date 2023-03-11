5d293071f6253e6444419a1436c59f79044a2e0b
yashkys <yashkhaitan02@gmail.com>
2023-03-11 12:32:15 +0530

    ui changes

--- app/src/main/java/com/example/youtube_clone/ui/library/LibraryFragment.java
+++ app/src/main/java/com/example/youtube_clone/ui/library/LibraryFragment.java
@@ -1,6 +1,33 @@
 package com.example.youtube_clone.ui.library;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.youtube_clone.databinding.FragmentLibraryBinding;
+
 public class LibraryFragment extends Fragment {
+
+    private FragmentLibraryBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        binding = FragmentLibraryBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
+    }
 }
--- app/src/main/java/com/example/youtube_clone/ui/subscriptions/SubscriptionsFragment.java
+++ app/src/main/java/com/example/youtube_clone/ui/subscriptions/SubscriptionsFragment.java
@@ -1,6 +1,33 @@
 package com.example.youtube_clone.ui.subscriptions;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class SubscriptionsFragment extends Fragment {
+import com.example.youtube_clone.databinding.FragmentSubscriptionsBinding;
+
+public class SubscriptionsFragment extends Fragment{
+
+    private FragmentSubscriptionsBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        binding = FragmentSubscriptionsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
+    }
 }
